6cd7c9e409c4b5ae8714057356b83f9efa5cc684
tray <onesimus.githua@student.moringaschool.com>
2023-11-27 10:47:35 +0300

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,14 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")    
+
+
+kamau = Person()
+kamau.talk()
+
+og = Person()
+og.walk()
